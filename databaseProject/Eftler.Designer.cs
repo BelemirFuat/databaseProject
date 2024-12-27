d7870c34e689d2be8d206db411e0b28493fe23d8
BelemirFuat <mtshbz@gmail.com>
2024-12-27 13:18:48 +0300

    eft yükleme, filtreleme ve güncelleme

--- databaseProject/Eftler.Designer.cs
+++ databaseProject/Eftler.Designer.cs
@@ -36,6 +36,8 @@
             button1 = new Button();
             button2 = new Button();
             anaMenuBtn = new Button();
+            button4 = new Button();
+            textBox5 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -61,7 +63,8 @@
             dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
             dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             dataGridView1.GridColor = Color.FromArgb(65, 69, 89);
-            dataGridView1.Location = new Point(22, 107);
+            dataGridView1.Location = new Point(25, 239);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
             dataGridView1.Name = "dataGridView1";
             dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = Color.FromArgb(65, 69, 89);
@@ -71,10 +74,11 @@
             dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
             dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.DefaultCellStyle.BackColor = Color.FromArgb(65, 69, 89);
             dataGridView1.RowTemplate.DefaultCellStyle.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             dataGridView1.RowTemplate.DefaultCellStyle.ForeColor = Color.White;
-            dataGridView1.Size = new Size(777, 477);
+            dataGridView1.Size = new Size(888, 540);
             dataGridView1.TabIndex = 6;
             // 
             // label1
@@ -82,10 +86,10 @@
             label1.AutoSize = true;
             label1.Font = new Font("Sitka Small", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Transparent;
-            label1.Location = new Point(489, 9);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(559, 12);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(230, 71);
+            label1.Size = new Size(288, 88);
             label1.TabIndex = 7;
             label1.Text = "EFTLER";
             // 
@@ -95,13 +99,14 @@
             button1.FlatStyle = FlatStyle.Popup;
             button1.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
             button1.ForeColor = Color.Red;
-            button1.Location = new Point(944, 203);
-            button1.Margin = new Padding(4);
+            button1.Location = new Point(1079, 271);
+            button1.Margin = new Padding(5);
             button1.Name = "button1";
-            button1.Size = new Size(167, 81);
+            button1.Size = new Size(191, 108);
             button1.TabIndex = 18;
-            button1.Text = "YENİ EFT GİR";
+            button1.Text = "Bilgileri Güncelle";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -109,10 +114,10 @@
             button2.FlatStyle = FlatStyle.Popup;
             button2.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
             button2.ForeColor = Color.Red;
-            button2.Location = new Point(944, 345);
-            button2.Margin = new Padding(4);
+            button2.Location = new Point(1079, 460);
+            button2.Margin = new Padding(5);
             button2.Name = "button2";
-            button2.Size = new Size(167, 81);
+            button2.Size = new Size(191, 108);
             button2.TabIndex = 19;
             button2.Text = "ÖDEMELERİ HESAPLA";
             button2.UseVisualStyleBackColor = false;
@@ -126,30 +131,61 @@
             anaMenuBtn.FlatStyle = FlatStyle.Popup;
             anaMenuBtn.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
             anaMenuBtn.ForeColor = Color.Red;
-            anaMenuBtn.Location = new Point(0, 598);
-            anaMenuBtn.Margin = new Padding(4);
+            anaMenuBtn.Location = new Point(0, 797);
+            anaMenuBtn.Margin = new Padding(5);
             anaMenuBtn.Name = "anaMenuBtn";
-            anaMenuBtn.Padding = new Padding(10);
-            anaMenuBtn.Size = new Size(1261, 48);
+            anaMenuBtn.Padding = new Padding(11, 13, 11, 13);
+            anaMenuBtn.Size = new Size(1441, 64);
             anaMenuBtn.TabIndex = 20;
             anaMenuBtn.Text = "ANA MENÜYE DÖN";
             anaMenuBtn.UseVisualStyleBackColor = false;
             anaMenuBtn.Click += anaMenuBtn_Click;
             // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(245, 224, 220);
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            button4.ForeColor = Color.Red;
+            button4.Location = new Point(234, 173);
+            button4.Margin = new Padding(5);
+            button4.Name = "button4";
+            button4.Size = new Size(191, 35);
+            button4.TabIndex = 26;
+            button4.Text = "FİLTRELEME";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.FromArgb(65, 69, 89);
+            textBox5.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            textBox5.ForeColor = Color.FromArgb(202, 158, 230);
+            textBox5.Location = new Point(48, 173);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "TC";
+            textBox5.Size = new Size(150, 31);
+            textBox5.TabIndex = 25;
+            // 
             // Eftler
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(69, 71, 90);
-            ClientSize = new Size(1261, 646);
+            ClientSize = new Size(1441, 861);
+            Controls.Add(button4);
+            Controls.Add(textBox5);
             Controls.Add(anaMenuBtn);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(dataGridView1);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Eftler";
             Text = "Eftler";
+            Load += Eftler_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -162,5 +198,7 @@
         private Button button1;
         private Button button2;
         private Button anaMenuBtn;
+        private Button button4;
+        private TextBox textBox5;
     }
 }
--- databaseProject/Eftler.cs
+++ databaseProject/Eftler.cs
@@ -2,25 +2,116 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace databaseProject
 {
     public partial class Eftler : Form
     {
+        private SQLiteConnection conn;  // SQLiteConnection formun bir üyesi olarak tanımlandı
+        private SQLiteDataAdapter adap; // DataAdapter tanımlandı
+        private DataSet ds;
         public Eftler()
         {
             InitializeComponent();
         }
+        private void loadData(string filter = "")
+        {
+            try
+            {
+                // Veritabanı bağlantısını başlat
+                conn = StartConnectionToDB();
+                conn.Open();
+                string lastFilter = "";
+                if (filter == "")
+                    lastFilter = "";
+                else
+                    lastFilter = "where " + filter;
 
+
+                // SQL sorgusu: Tablo verilerini çek
+                string query = "SELECT * FROM eftler " + lastFilter;
+
+                //MessageBox.Show(query); hayat kurtarıcı
+                // DataAdapter ve DataSet oluştur
+                adap = new SQLiteDataAdapter(query, conn);
+                ds = new DataSet();
+
+                // Verileri DataSet'e yükle
+                adap.Fill(ds, "eftler");
+
+                // DataGridView'i DataSet'e bağla
+                dataGridView1.DataSource = ds.Tables["eftler"];
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda mesaj göster
+                MessageBox.Show($"Hata: {ex.Message}");
+            }
+        }
         private void anaMenuBtn_Click(object sender, EventArgs e)
         {
             this.Close();
 
+        }
+
+        private void Eftler_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+        private SQLiteConnection StartConnectionToDB()
+        {
+            string relativePath = "..\\..\\..\\..\\projectDB.db";
+            string fullPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
+            string connectionString = $"Data Source={fullPath};Version=3;";
+            return new SQLiteConnection(connectionString);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string filter = "";
+
+            filter += $"ACIKLAMA like '{textBox5.Text}%' ";
+
+            loadData(filter);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Yeni veya değiştirilen satırları kontrol edin
+                DataTable changes = ds.Tables["eftler"].GetChanges();
+                if (changes != null)
+                {
+                    foreach (DataRow row in changes.Rows)
+                    {
+                        if (row.RowState == DataRowState.Added || row.RowState == DataRowState.Modified)
+                        {
+                            string blok = row["ACIKLAMA"].ToString();
+                            string oda = row["MIKTAR"].ToString();
+                        }
+                    }
+                }
+
+                // Eğer kontrol geçtiyse değişiklikleri kaydedin
+                SQLiteCommandBuilder cmdb1 = new SQLiteCommandBuilder(adap);
+                adap.Update(ds, "eftler");
+
+                // Başarı mesajı
+                MessageBox.Show("Değişiklikler başarıyla kaydedildi!");
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajı
+                MessageBox.Show($"Hata: {ex.Message}");
+            }
         }
     }
 }
