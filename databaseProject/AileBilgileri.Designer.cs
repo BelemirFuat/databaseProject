592e51f7b1c1ae6c28ddff2297511c153b511a44
BelemirFuat <mtshbz@gmail.com>
2024-12-27 11:26:59 +0300

    aile bilgileri formunda veri gösterme, filtreleme ve öğrenci ismini çekip yeni bir sutunda gösterme

--- databaseProject/AileBilgileri.Designer.cs
+++ databaseProject/AileBilgileri.Designer.cs
@@ -35,6 +35,14 @@
             label1 = new Label();
             button1 = new Button();
             anaMenuBtn = new Button();
+            button4 = new Button();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            textBox6 = new TextBox();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -60,7 +68,8 @@
             dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
             dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             dataGridView1.GridColor = Color.FromArgb(65, 69, 89);
-            dataGridView1.Location = new Point(37, 175);
+            dataGridView1.Location = new Point(42, 233);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
             dataGridView1.Name = "dataGridView1";
             dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = Color.FromArgb(65, 69, 89);
@@ -70,10 +79,11 @@
             dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
             dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.DefaultCellStyle.BackColor = Color.FromArgb(65, 69, 89);
             dataGridView1.RowTemplate.DefaultCellStyle.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             dataGridView1.RowTemplate.DefaultCellStyle.ForeColor = Color.White;
-            dataGridView1.Size = new Size(741, 434);
+            dataGridView1.Size = new Size(966, 579);
             dataGridView1.TabIndex = 6;
             // 
             // label1
@@ -81,9 +91,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Sitka Small", 36F, FontStyle.Bold | FontStyle.Italic);
             label1.ForeColor = SystemColors.ButtonFace;
-            label1.Location = new Point(411, 25);
+            label1.Location = new Point(470, 33);
             label1.Name = "label1";
-            label1.Size = new Size(453, 71);
+            label1.Size = new Size(570, 88);
             label1.TabIndex = 7;
             label1.Text = "AİLE İŞLEMLERİ";
             // 
@@ -93,10 +103,10 @@
             button1.FlatStyle = FlatStyle.Popup;
             button1.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
             button1.ForeColor = Color.Red;
-            button1.Location = new Point(946, 300);
-            button1.Margin = new Padding(4);
+            button1.Location = new Point(1070, 494);
+            button1.Margin = new Padding(5);
             button1.Name = "button1";
-            button1.Size = new Size(161, 73);
+            button1.Size = new Size(193, 97);
             button1.TabIndex = 8;
             button1.Text = "MESAJ GÖNDER";
             button1.UseVisualStyleBackColor = false;
@@ -110,29 +120,142 @@
             anaMenuBtn.FlatStyle = FlatStyle.Popup;
             anaMenuBtn.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
             anaMenuBtn.ForeColor = Color.Red;
-            anaMenuBtn.Location = new Point(0, 625);
-            anaMenuBtn.Margin = new Padding(4);
+            anaMenuBtn.Location = new Point(0, 833);
+            anaMenuBtn.Margin = new Padding(5);
             anaMenuBtn.Name = "anaMenuBtn";
-            anaMenuBtn.Padding = new Padding(10);
-            anaMenuBtn.Size = new Size(1268, 48);
+            anaMenuBtn.Padding = new Padding(11, 13, 11, 13);
+            anaMenuBtn.Size = new Size(1449, 64);
             anaMenuBtn.TabIndex = 18;
             anaMenuBtn.Text = "ANA MENÜYE DÖN";
             anaMenuBtn.UseVisualStyleBackColor = false;
             anaMenuBtn.Click += anaMenuBtn_Click;
             // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(245, 224, 220);
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            button4.ForeColor = Color.Red;
+            button4.Location = new Point(1002, 171);
+            button4.Margin = new Padding(5);
+            button4.Name = "button4";
+            button4.Size = new Size(191, 35);
+            button4.TabIndex = 24;
+            button4.Text = "FİLTRELEME";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.FromArgb(65, 69, 89);
+            textBox5.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            textBox5.ForeColor = Color.FromArgb(202, 158, 230);
+            textBox5.Location = new Point(867, 171);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "Adres";
+            textBox5.Size = new Size(114, 31);
+            textBox5.TabIndex = 23;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(65, 69, 89);
+            textBox4.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            textBox4.ForeColor = Color.FromArgb(202, 158, 230);
+            textBox4.Location = new Point(674, 171);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "BABA TEL";
+            textBox4.Size = new Size(114, 31);
+            textBox4.TabIndex = 22;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(65, 69, 89);
+            textBox3.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            textBox3.ForeColor = Color.FromArgb(202, 158, 230);
+            textBox3.Location = new Point(497, 171);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "ANNE TEL";
+            textBox3.Size = new Size(114, 31);
+            textBox3.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(65, 69, 89);
+            textBox2.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            textBox2.ForeColor = Color.FromArgb(202, 158, 230);
+            textBox2.Location = new Point(195, 171);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "ANNE AD";
+            textBox2.Size = new Size(114, 31);
+            textBox2.TabIndex = 20;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(65, 69, 89);
+            textBox1.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            textBox1.ForeColor = Color.FromArgb(202, 158, 230);
+            textBox1.Location = new Point(45, 171);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "TC ";
+            textBox1.Size = new Size(114, 31);
+            textBox1.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.FromArgb(65, 69, 89);
+            textBox6.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            textBox6.ForeColor = Color.FromArgb(202, 158, 230);
+            textBox6.Location = new Point(344, 171);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
+            textBox6.Name = "textBox6";
+            textBox6.PlaceholderText = "BABA AD";
+            textBox6.Size = new Size(114, 31);
+            textBox6.TabIndex = 25;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(245, 224, 220);
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold);
+            button2.ForeColor = Color.Red;
+            button2.Location = new Point(1070, 359);
+            button2.Margin = new Padding(5);
+            button2.Name = "button2";
+            button2.Size = new Size(193, 97);
+            button2.TabIndex = 26;
+            button2.Text = "DEĞİŞİKLİKLERİ KAYDET";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
             // AileBilgileri
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(69, 71, 90);
-            ClientSize = new Size(1268, 673);
+            ClientSize = new Size(1449, 897);
+            Controls.Add(button2);
+            Controls.Add(textBox6);
+            Controls.Add(button4);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(anaMenuBtn);
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(dataGridView1);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "AileBilgileri";
             Text = "AileBilgileri";
+            Load += AileBilgileri_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -144,5 +267,13 @@
         private Label label1;
         private Button button1;
         private Button anaMenuBtn;
+        private Button button4;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox6;
+        private Button button2;
     }
 }
--- databaseProject/AileBilgileri.cs
+++ databaseProject/AileBilgileri.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static databaseProject.Form1;
+
 
 namespace databaseProject
 {
     public partial class AileBilgileri : Form
     {
+        private SQLiteConnection conn;  // SQLiteConnection formun bir üyesi olarak tanımlandı
+        private SQLiteDataAdapter adap; // DataAdapter tanımlandı
+        private DataSet ds;
         public AileBilgileri()
         {
             InitializeComponent();
@@ -20,6 +26,185 @@ namespace databaseProject
         private void anaMenuBtn_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void AileBilgileri_Load(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn = StartConnectionToDB())
+            {
+                try
+                {
+                    // Bağlantıyı aç
+                    conn.Open();
+
+                    // SQL sorgusu: aile_bilgileri ile ogrenciBilgileri tablosunu birleştir ve öğrenci adını getir
+                    string query = @"
+                                    SELECT 
+                                        aile_bilgileri.*,
+                                        ogrenciBilgileri.ADI AS OgrenciAdi
+                                    FROM 
+                                        aile_bilgileri
+                                    LEFT JOIN 
+                                        ogrenciBilgileri
+                                    ON 
+                                        aile_bilgileri.TC = ogrenciBilgileri.TC";
+
+                    // Sorgu sonuçlarını tutmak için DataTable oluştur
+                    DataTable dataTable = new DataTable();
+
+                    // SQLiteCommand ve SQLiteDataAdapter ile sorguyu çalıştır
+                    using (SQLiteCommand command = new SQLiteCommand(query, conn))
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(command))
+                    {
+                        // Sorgu sonuçlarını DataTable'a doldur
+                        adapter.Fill(dataTable);
+                    }
+
+                    // DataGridView'e bağla
+                    dataGridView1.DataSource = dataTable;
+
+                    // Kullanıcı dostu kolon adları ayarlanabilir
+                    dataGridView1.Columns["OgrenciAdi"].HeaderText = "Öğrenci Adı";
+                }
+                catch (Exception ex)
+                {
+                    // Hata durumunda mesaj göster
+                    MessageBox.Show($"Hata: {ex.Message}");
+                }
+            }
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //FILTRELEME
+            string filter = "";
+            int count = 0;
+            if (textBox1.Text.Length > 0)
+                count++;
+            if (textBox2.Text.Length > 0)
+                count++;
+
+            if (textBox3.Text.Length > 0)
+                count++;
+
+            if (textBox4.Text.Length > 0)
+                count++;
+
+            if (textBox5.Text.Length > 0)
+                count++;
+            if (textBox6.Text.Length > 0)
+                count++;
+
+
+
+            if (textBox1.Text.Length > 0)
+            {
+                filter += $"TC like '{textBox1.Text}%' ";
+                if (--count > 0)
+                    filter += " AND ";
+
+            }
+            if (textBox2.Text.Length > 0)
+            {
+                filter += $"ANNEAD = '{textBox2.Text}%' ";
+                if (--count > 0)
+                    filter += " AND ";
+
+            }
+            if (textBox3.Text.Length > 0)
+            {
+
+                filter += $"ANNETELEFON like '{textBox3.Text}%' ";
+                if (--count > 0)
+                    filter += " AND ";
+            }
+            if (textBox4.Text.Length > 0)
+            {
+
+                filter += $"BABATELEFON like '{textBox4.Text}%' ";
+                if (--count > 0)
+                    filter += " AND ";
+            }
+            if (textBox5.Text.Length > 0)
+            {
+
+                filter += $"ADRES like '{textBox5.Text}%' ";
+                if (--count > 0)
+                    filter += " AND ";
+            }
+            if (textBox6.Text.Length > 0)
+            {
+
+                filter += $"BABAAD like '{textBox6.Text}%' ";
+                if (--count > 0)
+                    filter += " AND ";
+            }
+
+
+            loadData(filter);
+
+        }
+        private void loadData(string filter = "")
+        {
+            try
+            {
+                // Veritabanı bağlantısını başlat
+                conn = StartConnectionToDB();
+                conn.Open();
+                string lastFilter = "";
+                if (filter == "")
+                    lastFilter = "";
+                else
+                    lastFilter = "where " + filter;
+
+
+                // SQL sorgusu: Tablo verilerini çek
+                string query = "SELECT * FROM aile_bilgileri " + lastFilter;
+
+                //MessageBox.Show(query); hayat kurtarıcı
+                // DataAdapter ve DataSet oluştur
+                adap = new SQLiteDataAdapter(query, conn);
+                ds = new DataSet();
+
+                // Verileri DataSet'e yükle
+                adap.Fill(ds, "aile_bilgileri");
+
+                // DataGridView'i DataSet'e bağla
+                dataGridView1.DataSource = ds.Tables["aile_bilgileri"];
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda mesaj göster
+                MessageBox.Show($"Hata: {ex.Message}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Yeni veya değiştirilen satırları kontrol edin
+                DataTable changes = ds.Tables["aile_bilgileri"].GetChanges();
+                
+
+                // Eğer kontrol geçtiyse değişiklikleri kaydedin
+                SQLiteCommandBuilder cmdb1 = new SQLiteCommandBuilder(adap);
+                adap.Update(ds, "aile_bilgileri");
+
+                // Başarı mesajı
+                MessageBox.Show("Değişiklikler başarıyla kaydedildi!");
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajı
+                MessageBox.Show($"Hata: {ex.Message}");
+            }
         }
     }
 }
